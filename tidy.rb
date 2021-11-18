5958c521d25f74ea947350a163fbbd7d1016e81f
Scott Moore <hello@scottcmoore.com>
2021-11-17 23:38:16 -0700

    RuboCop
    Install RuboCop and lint the script.

--- tidy.rb
+++ tidy.rb
@@ -1,18 +1,17 @@
+# frozen_string_literal: true
+
+# Class to clean up the worklog
 class Tidy
-
   def print_if_unfinished(line)
-		if line.include?("[ ]") || line.include?("[-]")
-			puts line
-		end
-	end
+    puts line if line.include?('[ ]') || line.include?('[-]')
+  end
 
-	def initialize
-		puts "initializing"
-		file = File.open("worklog_copy.md")
-		file_data = file.readlines.each { |line| self.print_if_unfinished(line) }
-		file.close
-	end
-
+  def initialize
+    puts 'initializing'
+    file = File.open('worklog_copy.md')
+    file.readlines.each { |line| print_if_unfinished(line) }
+    file.close
+  end
 end
 
 Tidy.new
